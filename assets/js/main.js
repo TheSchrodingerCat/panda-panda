1065f51c2b41a337c5a61da091baa92a72171e1d
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-20 04:31:46 -0600

    cambios al css2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ function principal(){
 
 	var fotos = document.getElementById("fotitos");
 
-	//creamos divs
+	//creamos divs para fotos
 	arrFotos.forEach(function(element){
 		var cajita = document.createElement("div");
 		cajita.setAttribute("class","pictures");
@@ -15,11 +15,25 @@ function principal(){
 		fotito.setAttribute("id","pict" + arrFotos.indexOf(element));
 		fotito.setAttribute("src",element);
 
-
-
 		cajita.appendChild(fotito);
 		fotos.appendChild(cajita);
 	});
+
+	//creamos botones
+	var arrBotones = ["Origen","Extinción","Restaurar Fotos"];
+	var botones = document.getElementById("botones");
+
+	arrBotones.forEach(function(element){
+		var btn = document.createElement("button");
+		btn.setAttribute("class","btn");
+		btn.setAttribute("id","btn" + arrBotones.indexOf(element));
+		var nameBoton = document.createTextNode(element);
+
+		btn.appendChild(nameBoton);
+		botones.appendChild(btn);
+	});
+
+	
 
 }
 principal();
